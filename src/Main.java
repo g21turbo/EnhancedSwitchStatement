a1b35376b3f837776416fe1b43c9149795d1ef6f
g21turbo <dg21stew@gmail.com>
2023-10-25 20:48:34 -0700

    update the getQuarter method using the yield keyword

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String month = "bgfdbMarch";
+        String month = "March";
         System.out.println("The month of " + month + " is in the " + getQuarter(month));
 
         int switchValue = 6;
@@ -23,17 +23,14 @@ public class Main {
     public static String getQuarter(String month) {
 
         return switch (month) {
-            case "January", "February", "March" -> "first quarter";
-            case "April", "May", "June" -> "second quarter";
-            case "July", "August", "September" -> "third quarter";
-            case "October", "November", "December" -> "fourth quarter";
+            case "January", "February", "March" -> { yield "1st"; }
+            case "April", "May", "June" -> { yield "2nd"; }
+            case "July", "August", "September" -> { yield "3rd"; }
+            case "October", "November", "December" -> { yield "4th"; }
             default ->  {
                 String badResponse = month + " is bad";
                 yield badResponse;
             }
-
         };
-
     }
-
 }
